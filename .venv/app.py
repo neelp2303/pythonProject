02a8bb1cc9a775830ebe905cf8684b3893e072cb
neelp2303 <neelp6475@gmail.com>
2024-01-17 13:18:10 +0530

    Sets

--- .venv/app.py
+++ .venv/app.py
@@ -4,7 +4,8 @@ print("hello world")
 # print(patient,type)
 # age=input("Enter your birth year :" )
 # print(2024-int(age))
-# Code for calculator program
+
+#/----  Code for calculator program
 # num1=int(input("Enter a num : "))
 # num2=int(input("Enter a num : "))
 # option = int(input("Enter option 1-add \n2-sub \n3-mul \n4-div "))
@@ -18,7 +19,7 @@ print("hello world")
 #     print(num2/num1)
 # else:print("Enter valid number")
 
-#for loop,while loop,lists
+#/---- for loop,while loop,lists
 # num=[1,2,3]
 # num.reverse()
 # print(num)
@@ -29,17 +30,26 @@ print("hello world")
 #     print(num[i])
 #     i +=1
 
-#FStrings
+#/---- FStrings
 # st="Hello {}"
 # name =input("Enter name")
 # print(f"Hello {name}")
 
-#Functions and recursion
-def factorial(a):
-    if (a==0 or a==1):
-        return (a)
-    else:
-        return (a * factorial(a-1))
+#/---- Functions and recursion
+# def factorial(a):
+#     if (a==0 or a==1):
+#         return (a)
+#     else:
+#         return (a * factorial(a-1))
+#
+# fact=factorial(5)
+# print(fact)
 
-fact=factorial(5)
-print(fact)
+#/---- Sets
+# s1={1,2,3,4}
+# s2={4,5,6}
+# print(s1.intersection(s2))
+# print(s1.symmetric_difference(s2))
+# print(s1.union(s2))
+# s1.update(s2)
+# print(s1)
